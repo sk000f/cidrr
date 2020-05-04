3aee96cbd0ece99b1d9a06312f7bb9d62808bdc7
sk000f <mike@skaife.me>
2020-05-04 18:56:24 +0100

    update IP and subnet parsing to use a regex

--- int/cidr/cidr.go
+++ int/cidr/cidr.go
@@ -1,4 +1,20 @@
 package cidr
 
+import (
+	"regexp"
+)
+
+const subnetPattern = "^(25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])\\/\\d+$"
+
 // Parse returns true if specified subnet is valid CIDR notation
-func Parse(subnet string) bool { return true }
+func Parse(subnet string) (bool, error) {
+
+	re, err := regexp.Compile(subnetPattern)
+	if err != nil {
+		return false, err
+	}
+
+	r := re.MatchString(subnet)
+
+	return r, nil
+}
--- int/cidr/cidr_test.go
+++ int/cidr/cidr_test.go
@@ -6,13 +6,45 @@ import (
 	"github.com/sk000f/cidrr/int/cidr"
 )
 
-func TestParse(t *testing.T) {
-	t.Run("192.168.1.0/32 is valid subnet", func(t *testing.T) {
-		got := cidr.Parse("192.168.1.0/32")
-		want := true
+var parseTests = []struct {
+	name  string
+	input string
+	want  bool
+	err   error
+}{
+	{
+		"192.168.1.0/32 is a valid IP and subnet",
+		"192.168.1.0/32",
+		true,
+		nil,
+	},
+	{
+		"256.256.256.256/256 is not a valid IP and subnet",
+		"256.256.256.256/256",
+		false,
+		nil,
+	},
+	{
+		"255.255.255.250/3 is a valid IP and subnet",
+		"255.255.255.250/3",
+		true,
+		nil,
+	},
+}
 
-		if got != want {
-			t.Errorf("got %v; want %v", got, want)
-		}
-	})
+func TestParse(t *testing.T) {
+	for _, tt := range parseTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := cidr.Parse(tt.input)
+
+			if err != nil {
+
+			}
+
+			if got != tt.want {
+				t.Errorf("got %v; want %v", got, tt.want)
+			}
+		})
+	}
+
 }
